59ef13954f566f638882985cac4e03a2452ee44f
JavierMorgado <javier.morgado.burgos@gmail.com>
2025-03-27 11:06:57 +0100

    Cambios en detalles estéticos de botones, inputs y textos.
    Maquetado el perfil de empresa.

--- src/LogIn-Prof.jsx
+++ src/LogIn-Prof.jsx
@@ -1,47 +1,63 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import agylelogo from './assets/agyleICT.png';
-import viteLogo from '/vite.svg';
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import agylelogo from "./assets/agyleICT.png";
+import viteLogo from "/vite.svg";
 
 export default function LogInProf(props) {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    function goToRegisterProf() {
-        window.location.href = '/register-profesional';
-    }
+  function goToRegisterProf() {
+    window.location.href = "/register-profesional";
+  }
 
-    return (
-        <div className="d-flex flex-column justify-content-start align-items-center vh-100 vw-100">
-            <div className="d-flex justify-content-end align-items-center pt-3 me-3 w-50">
-                <h1>INICIO DE SESIÓN PARA PROFESIONALES</h1>
-            </div>
+  return (
+    <div className="d-flex flex-column justify-content-start align-items-center vh-100 vw-100">
+      <div className="d-flex justify-content-end align-items-center pt-3 me-3 w-50">
+        <h1>INICIO DE SESIÓN PARA PROFESIONALES</h1>
+      </div>
 
-            <div className="rounded-5 d-flex flex-column justify-content-center align-items-center" style={{ backgroundColor: '#D83000', width: '75%', height: '50%' }}>
-                <h4 className="mb-4">INICIA SESIÓN CON TU <br /> CORREO Y CONTRASEÑA</h4>
+      <div
+        className="rounded-5 d-flex flex-column justify-content-center align-items-center"
+        style={{ backgroundColor: "#D83000", width: "75%", height: "50%" }}
+      >
+        <h4 className="mb-4">
+          INICIA SESIÓN CON TU <br /> CORREO Y CONTRASEÑA
+        </h4>
 
-                <div className="mb-5 d-flex">
-                    <input
-                        type="email"
-                        placeholder="menganito.perez@gmail.com"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
+        <div className="mb-5 d-flex" style={{ gap: "1rem" }}>
+          <input
+            type="email"
+            placeholder="menganito.perez@gmail.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
 
-                    <input
-                        type="password"
-                        placeholder="contraseña123"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                </div>
-
-                <h4 className="mb-4">SI NO TIENES CUENTA REGÍSTRATE AQUÍ</h4>
-
-                <button onClick={goToRegisterProf} className="rounded-pill" style={{ width: '150px', height: '40px', backgroundColor: '#fff', letterSpacing: '0.5px', color: '#D83000', textAlign: 'center' }}>
-                    REGÍSTRATE
-                </button>
-            </div>
+          <input
+            type="password"
+            placeholder="contraseña123"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
-    );
+
+        <h4 className="mb-4">SI NO TIENES CUENTA REGÍSTRATE AQUÍ</h4>
+
+        <button
+          onClick={goToRegisterProf}
+          className="rounded-pill"
+          style={{
+            width: "150px",
+            height: "40px",
+            backgroundColor: "#fff",
+            letterSpacing: "0.5px",
+            color: "#D83000",
+            textAlign: "center",
+          }}
+        >
+          REGÍSTRATE
+        </button>
+      </div>
+    </div>
+  );
 }
--- src/Register-Empresa.jsx
+++ src/Register-Empresa.jsx
@@ -1,155 +1,159 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import agylelogo from './assets/agyleICT.png'
-import viteLogo from '/vite.svg'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import agylelogo from "./assets/agyleICT.png";
+import viteLogo from "/vite.svg";
 
+export default function RegisterEmpresa(props) {
+  const [formData, setFormData] = useState({
+    nombre: "",
+    email: "",
+    password: "",
+    movil: "",
+    plan: "",
+  });
 
-export default function RegisterEmpresa(props){
-    const [formData, setFormData] = useState({
-        nombre: '',
-        email: '',
-        password: '',
-        movil: '',
-        plan: ''
-    })
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({ ...formData, [name]: value });
-    };
+  const handlePlanSelect = (plan) => {
+    setFormData({ ...formData, plan });
+  };
 
-    const handlePlanSelect = (plan) => {
-        setFormData({ ...formData, plan });
-    };
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if(!formData.nombre || !formData.email || !formData.password || !formData.movil || !formData.plan){
-            alert('Por favor, rellene todos los campos');
-            return;
-        }
-
-        //Aqui puedes enviar los datos al backend
-        console.log('Datos enviados:', formData);
-        alert('Registro exitoso');
-    };
-
-
-
-    function goToEmpresa(){
-        window.location.href = '/miEmpresa';
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (
+      !formData.nombre ||
+      !formData.email ||
+      !formData.password ||
+      !formData.movil ||
+      !formData.plan
+    ) {
+      alert("Por favor, rellene todos los campos");
+      return;
     }
 
-    return(
-        <div className="d-flex flex-column justify-content-start align-items-center vh-100 vw-100">
-            <h1>REGÍSTRATE COMO  <br /> EMPRESA</h1>
+    //Aqui puedes enviar los datos al backend
+    console.log("Datos enviados:", formData);
+    alert("Registro exitoso");
+  };
 
-            <form
-                onSubmit={handleSubmit}
-                className='btext-white rounded-4 mt-4 p-4 d-flex flex-column align-items-center'
-                style={{backgroundColor: '#002C4B', width: '50%'}}
-            >
-                
-                <h5 className="text-uppercase mb-4">
-                    Datos de la empresa
-                </h5>
+  function goToEmpresa() {
+    window.location.href = "/miEmpresa";
+  }
 
-                {/* FALTA CREAR IMG */}
+  return (
+    <div className="d-flex flex-column justify-content-start align-items-center vh-100 vw-100">
+      <h1>
+        REGÍSTRATE COMO <br /> EMPRESA
+      </h1>
 
-                <div className="mb-3 w-100">
-                    <h6 className="text-uppercase">Nombre</h6>
-                    <input
-                        type="text"
-                        name="nombre"
-                        value={formData.nombre}
-                        onChange={handleChange}
-                        placeholder="Ej. AgyleICT"
-                    />
-                </div>
-            
-                <div className="mb-3 w-100">
-                    <h6 className="text-uppercase">Correo</h6>
-                    <input
-                        type="email"
-                        name="correo"
-                        value={formData.email}
-                        onChange={handleChange}
-                        placeholder="rh@agyleict.com"
-                    />
-                </div>
+      <form
+        onSubmit={handleSubmit}
+        className="btext-white rounded-4 mt-4 p-4 d-flex flex-column align-items-center"
+        style={{ backgroundColor: "#002C4B", width: "50%" }}
+      >
+        <h5 className="text-uppercase mb-4">Datos de la empresa</h5>
 
-                
-                <div className="mb-4 w-100">
-                <h6 className="text-uppercase">Móvil</h6>
-                    <input
-                        type="text"
-                        name="movil"
-                        value={formData.movil}
-                        onChange={handleChange}
-                        placeholder="91 654 55 45"
-                    />
-                </div>
+        {/* FALTA CREAR IMG */}
 
-
-                <h6 className="fw-semibold text-uppercase text-center mt-2 mb-4" style={{ letterSpacing: '1px' }}>
-                    ¿A qué nivel te quieres suscribir?
-                </h6>
-
-                
-                <div className="d-flex flex-column flex-md-row justify-content-between w-100 gap-3 mb-4">
-                    <div
-                        title='Bronce'
-                        className={`rounded-3 p-3 flex-fill text-center border ${
-                            formData.plan === 'Bronce' ? 'border-warning bg-opacity-100' : 'bg-opacity-50'
-                        }`}
-                        onClick={() => handlePlanSelect('Bronce')}
-                        style={{ cursor: 'pointer', backgroundColor: '#CD7F32'}}
-                    >
-                        <h6 className="fw-bold">BRONCE</h6>
-                        <p className="small mb-0" style={{color: 'black' }}> 
-                            AgyleICT gestiona el reclutamiento de tus profesionales
-                        </p>
-                    </div>
-
-                    <div
-                        title='Plata'
-                        className={`rounded-3 p-3 flex-fill text-center border ${
-                            formData.plan === 'Plata' ? 'border-secondary bg-opacity-100' : 'bg-opacity-50'
-                        }`}
-                        onClick={() => handlePlanSelect('Plata')}
-                        style={{ cursor: 'pointer', backgroundColor: '#C0C0C0' }}
-                    >
-                        <h6 className="fw-bold">PLATA</h6>
-                        <p className="small mb-0" style={{color: 'black' }}>
-                            AgyleICT gestiona el reclutamiento <br />
-                            Branding en tu nombre para suscritos
-                        </p>
-                    </div>
-
-
-                    <div
-                        title='Oro'
-                        className={`rounded-3 p-3 flex-fill text-center border ${
-                            formData.plan === 'Oro' ? 'border-warning' : 'bg-opacity-75'
-                        }`}
-                        onClick={() => handlePlanSelect('Oro')}
-                        style={{ cursor: 'pointer', backgroundColor: '#EFB810'}}
-                    >
-                        <h6 className="fw-bold">ORO</h6>
-                        <p className="small mb-0" style={{color: 'black' }}>
-                            Todo lo anterior <br />
-                            + Eventos de reclutamiento para tu perfil
-                        </p>
-                    </div>
-                </div>
-                <button onClick={goToEmpresa} type="submit" className="btn btn-light rounded-pill px-4 fw-semibold">
-                        Registrarse
-                </button>
-            </form>
-            
-            
+        <div className="mb-3 w-100">
+          <h6 className="text-uppercase">Nombre</h6>
+          <input
+            type="text"
+            name="nombre"
+            value={formData.nombre}
+            onChange={handleChange}
+            placeholder="Ej. AgyleICT"
+          />
         </div>
 
-        
-    )
+        <div className="mb-3 w-100">
+          <h6 className="text-uppercase">Correo</h6>
+          <input
+            type="email"
+            name="correo"
+            value={formData.email}
+            onChange={handleChange}
+            placeholder="rh@agyleict.com"
+          />
+        </div>
+
+        <div className="mb-4 w-100">
+          <h6 className="text-uppercase">Móvil</h6>
+          <input
+            type="text"
+            name="movil"
+            value={formData.movil}
+            onChange={handleChange}
+            placeholder="91 654 55 45"
+          />
+        </div>
+
+        <h6
+          className="fw-semibold text-uppercase text-center mt-2 mb-4"
+          style={{ letterSpacing: "1px" }}
+        >
+          ¿A qué nivel te quieres suscribir?
+        </h6>
+
+        <div className="d-flex flex-column flex-md-row justify-content-between w-100 gap-3 mb-4">
+          <div
+            title="Bronce"
+            className={`rounded-3 p-3 flex-fill text-center border ${
+              formData.plan === "Bronce"
+                ? "border-warning bg-opacity-100"
+                : "bg-opacity-50"
+            }`}
+            onClick={() => handlePlanSelect("Bronce")}
+            style={{ cursor: "pointer", backgroundColor: "#CD7F32" }}
+          >
+            <h6 className="fw-bold">BRONCE</h6>
+            <p className="small mb-0" style={{ color: "black" }}>
+              AgyleICT gestiona el reclutamiento de tus profesionales
+            </p>
+          </div>
+
+          <div
+            title="Plata"
+            className={`rounded-3 p-3 flex-fill text-center border ${
+              formData.plan === "Plata"
+                ? "border-secondary bg-opacity-100"
+                : "bg-opacity-50"
+            }`}
+            onClick={() => handlePlanSelect("Plata")}
+            style={{ cursor: "pointer", backgroundColor: "#C0C0C0" }}
+          >
+            <h6 className="fw-bold">PLATA</h6>
+            <p className="small mb-0" style={{ color: "black" }}>
+              AgyleICT gestiona el reclutamiento <br />
+              Branding en tu nombre para suscritos
+            </p>
+          </div>
+
+          <div
+            title="Oro"
+            className={`rounded-3 p-3 flex-fill text-center border ${
+              formData.plan === "Oro" ? "border-warning" : "bg-opacity-75"
+            }`}
+            onClick={() => handlePlanSelect("Oro")}
+            style={{ cursor: "pointer", backgroundColor: "#EFB810" }}
+          >
+            <h6 className="fw-bold">ORO</h6>
+            <p className="small mb-0" style={{ color: "black" }}>
+              Todo lo anterior <br />+ Eventos de reclutamiento para tu perfil
+            </p>
+          </div>
+        </div>
+        <button
+          onClick={goToEmpresa}
+          type="submit"
+          className="btn btn-light rounded-pill px-4 fw-semibold"
+        >
+          Registrarse
+        </button>
+      </form>
+    </div>
+  );
 }
