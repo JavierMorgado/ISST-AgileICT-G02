c981de1659fb65c5783a290fd993fd6a0215f102
InesBerlanga <ines.berlanga@alumnos.upm.es>
2025-03-30 12:55:03 +0200

    Merge branch 'main' of https://github.com/JavierMorgado/ISST-AgileICT-G02
    Co-authored-by: JavierMorgado <JavierMorgado@users.noreply.github.com>

---
+++ src/MainMenu.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import { Row, Stack, Button, Form, Col, Container } from 'react-bootstrap';
+
+
+export default function MainMenu(props){
+    const handleGoHome = () => {
+        window.location.href = '/';
+    };
+
+    const handleLogout = () => {
+        // Add your logout logic here
+        console.log('Logged out');
+    };
+
+    return (
+        <div>
+            <button onClick={handleGoHome} className='white rounded-pill'>Volver al Inicio</button>
+            <button onClick={handleLogout} className='white rounded-pill'>Log Out</button>
+        </div>
+    )
+}
--- src/Perfil-prof.jsx
+++ src/Perfil-prof.jsx
@@ -1,49 +1,72 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import agylelogo from './assets/agyleICT.png'
-import viteLogo from '/vite.svg'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import agylelogo from "./assets/agyleICT.png";
+import viteLogo from "/vite.svg";
 
-export default function PerfilProf(props){
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+export default function PerfilProf(props) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    function goToOfertas() {
-        window.location.href = '/miPerfil';
-    }
+  function goToOfertas() {
+    window.location.href = "/miPerfil";
+  }
 
-    return (
-        <div className="d-flex flex-column justify-content-start align-items-center vh-100 vw-100">
-            <div className="d-flex justify-content-between align-items-center pt-3 w-100 px-4">
-                <div></div> {/* Espacio vacío para mantener el título centrado */}
-                <h1 className="mx-auto">MI PERFIL</h1>
-                <button onClick={goToOfertas} className="rounded-pill" 
-                    style={{width: '150px', height: '40px', backgroundColor: '#fff', letterSpacing: '0.5px', color: '#002C4B', textAlign: 'center',}}
-                > VER OFERTAS
-                </button>
-            </div>
+  return (
+    <div className="d-flex flex-column justify-content-start align-items-center vh-100 vw-100">
+      <div className="d-flex justify-content-center align-items-center pt-3 w-100 mb-3 position-relative">
+        <h1 className="text-center">MI PERFIL</h1>
+        <button
+          onClick={goToOfertas}
+          className="rounded-pill position-absolute botonopcion"
+          style={{
+            right: "15%", // Ajusta la posición del botón a la derecha
+            width: "150px",
+            height: "40px",
+            backgroundColor: "#002C4B",
+            letterSpacing: "0.5px",
+            color: "white !important",
+            textAlign: "center",
+          }}
+        >
+          VER OFERTAS
+        </button>
+      </div>
 
-            <div className="rounded-5 d-flex flex-column justify-content-center align-items-center" style={{ backgroundColor: '#D83000', width: '75%', height: '50%' }}>
-                <div className="mb-3 w-100">
-                    <h5 className="mb-4">NOMBRE</h5>
-                    <h6 className="mb-4">Menganito</h6> {/*props o coger de la base de datos??*/}
-                </div>
-                
-                <div className="mb-3 w-100">
-                    <h5 className="mb-4">PUESTO</h5>
-                    <h6 className="mb-4">Jefe de Producto</h6>
-                </div>
+      <div
+        className="rounded-5 d-flex flex-column justify-content-center align-items-center"
+        style={{
+          backgroundColor: "#D83000",
+          width: "75%",
+          height: "auto",
+          padding: "2rem",
+        }}
+      >
+        {/* FALTA CREAR IMG */}
 
-                <div className="mb-3 w-100">
-                    <h5 className="mb-4">CUALIDADES TÉCNICAS</h5>
-                    <h6 className="mb-4">C++</h6>
-                </div>
-
-                <div className="mb-3 w-100">
-                    <h5 className="mb-4">DISPONIBILIDAD</h5>
-                    <h6 className="mb-4">10/10/2025 - 10/10/2026</h6>
-                </div>
-
-            </div>
+        {/* NOMBRE */}
+        <div className="mb-3 w-100 text-center">
+          <h5 className="mb-2">NOMBRE</h5>
+          <h6 className="mb-4">Menganito</h6>
         </div>
-    );
+
+        {/* PUESTO */}
+        <div className="mb-3 w-100 text-center">
+          <h5 className="mb-2">PUESTO</h5>
+          <h6 className="mb-4">Jefe de Producto</h6>
+        </div>
+
+        {/* CUALIDADES TÉCNICAS */}
+        <div className="mb-3 w-100 text-center">
+          <h5 className="mb-2">CUALIDADES TÉCNICAS</h5>
+          <h6 className="mb-4">C++</h6>
+        </div>
+
+        {/* DISPONIBILIDAD */}
+        <div className="mb-3 w-100 text-center">
+          <h5 className="mb-2">DISPONIBILIDAD</h5>
+          <h6 className="mb-4">10/10/2025 - 10/10/2026</h6>
+        </div>
+      </div>
+    </div>
+  );
 }
